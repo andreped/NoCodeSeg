e94c59d6004dc927ce3dd3ffdea39732bb559953
andreped <andrped94@gmail.com>
2022-01-06 16:08:17 +0100

    fixed scaling issue when importing - support for setting downsampling

--- source/importPyramidalTIFF.groovy
+++ source/importPyramidalTIFF.groovy
@@ -5,21 +5,24 @@
  * 
  * Furthermore, we assume that the image is uint8, where each uint correspond to a different class (currently only supports one class, value != 0)
  *
- * Code is slightly modified from Pete Bankhead's script to support RunForProject:
+ * Code is slightly modified from Peter Bankhead's script to support RunForProject:
  * https://gist.github.com/petebankhead/27c1f8cd950583452c756f3a2ea41fc0
  *
  * which was written for https://forum.image.sc/t/rendering-wsi-as-overlay-on-top-of-another-wsi/52629/25?u=andreped
+ *
+ * All credit to Peter Bankhead and the QuPath team for their tremendous support in implementing this script!
  *
  * @author André Pedersen
  */
 
 
- // --- SET THESE PARAMETERS ---
-def masksPath = "C:/path-to-masks-dir"  // path to where TIFFs are stored
+// --- SET THESE PARAMETERS ---
+def masksPath = "C:/Users/andrp/workspace/FP_projects/test6/results/"  // path to where TIFFs are stored
+def downsample = 4                      // which scaling factor to use, if the segmentation is produced at a lower magnification level
 def level = 0                           // which level to extract segmentation from (choosing 0 may be slow)
 def extension = ".tiff"                 // pyramidal TIFF
 def className = "Epithelium"            // name of class of interest
-double threshold = 0.5                  // Threshold value
+double threshold = 0.5                  // threshold value
 int channel = 0                         // 0-based index for the channel to threshold
 boolean fromFP = true                   // if TIFF is created in FastPathology, should set this to 'true'
 // ----------------------------
@@ -56,11 +59,23 @@ if (level != 0) {
 }
 
 // Create a thresholded image
-def thresholdServer = PixelClassifierTools.createThresholdServer(server, channel, threshold, belowClass, aboveClass)
+def thresholdServer = PixelClassifierTools.createThresholdServer(server, channel, threshold, belowClass, getPathClass("temporary"))
 
 // Create annotations and add to the current object hierarchy
 def hierarchy = getCurrentHierarchy()
 PixelClassifierTools.createAnnotationsFromPixelClassifier(hierarchy, thresholdServer, -1, -1)
+
+// Select current annotations
+selectObjects {it.isAnnotation()}
+
+// Get current annotations, rescale
+def oldObjects = getAnnotationObjects()
+def transform = java.awt.geom.AffineTransform.getScaleInstance(downsample, downsample)
+def newObjects = oldObjects.collect {p -> PathObjectTools.transformObject(p, transform, false)}
+addObjects(newObjects)
+
+// Delete old annotations
+clearSelectedObjects(false);
 
 print "Done!"
 
