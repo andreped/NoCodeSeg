56c701bff6ff92fcbcd4da050516c883533daaa8
andreped <andrped94@gmail.com>
2022-06-09 15:40:30 +0200

    swapped JS with python solution for download count

---
+++ assets/generate_badge.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+
+path = "./filedownloads.csv"
+data = np.asarray(pd.read_csv(path, delimiter=None))
+pid2find = "10.18710/TLA01U"
+
+res = []
+total = 0
+for i in range(data.shape[0]):
+	id_, pid_, count_ = data[i]
+	if pid2find in pid_:
+		total += count_
+
+ret = '<a href="https://doi.org/' + \
+          pid2find + \
+          '"><img src="https://img.shields.io/badge/DataverseNO%20downloads-' + \
+          str(total) + \
+          '-orange"></a>'
+
+print("generated html line: ", ret)
+
+with open("test.md", "w") as f:
+	f.write(ret)
