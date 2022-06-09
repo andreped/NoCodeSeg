627861d4d610709a5f7df08446856ee58b7a00c8
André Pedersen <andrped94@gmail.com>
2022-06-09 14:36:57 +0200

    added async

--- source/html/generate-badge.js
+++ source/html/generate-badge.js
@@ -13,12 +13,13 @@
 //    a.download = name;
 //}
 
-function fetchText() {
+async function fetchText() {
+    //const { fetch } = await import('node-fetch');  // dynamic import don't work as it should with node-fetch?
     const { default: fetch } = await import('node-fetch')
     const { JSDOM } = require("jsdom");
     const { default: document } = (new JSDOM(`...`)).window;
     var fs = require('fs')
-    
+
     let url = "https://dataverse.no/api/info/metrics/filedownloads?parentAlias=ntnu";
     let response = await fetch(url);
     if (response.status === 200) {
