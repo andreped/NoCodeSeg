e109d0502e217534d2e72241e33dbffee0b5d5ac
andreped <andrped94@gmail.com>
2022-06-21 19:41:21 +0200

    works. Now adjust height and width of PNG

--- assets/generate_badge.py
+++ assets/generate_badge.py
@@ -5,8 +5,8 @@ import imgkit
 
 options = {
     'format': 'png',
-    'crop-h': '5',
-    'crop-w': '5',
+    'crop-h': '30',
+    'crop-w': '70',
     'crop-x': '5',
     'crop-y': '5',
     'encoding': "UTF-8",
