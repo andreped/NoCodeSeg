6c61bc0bf14f83ee6c4bd96d436567b00c8fae48
andreped <andrped94@gmail.com>
2022-06-21 19:33:56 +0200

    add cropping options around generated PNG

--- assets/generate_badge.py
+++ assets/generate_badge.py
@@ -3,6 +3,24 @@ import pandas as pd
 import imgkit
 
 
+options = {
+    'format': 'png',
+    'crop-h': '5',
+    'crop-w': '5',
+    'crop-x': '5',
+    'crop-y': '5',
+    'encoding': "UTF-8",
+    'custom-header' : [
+        ('Accept-Encoding', 'gzip')
+    ]
+    'cookie': [
+        ('cookie-name1', 'cookie-value1'),
+        ('cookie-name2', 'cookie-value2'),
+    ],
+    'no-outline': None
+}
+
+
 path = "filedownloads?parentAlias=ntnu"
 data = np.asarray(pd.read_csv(path, delimiter=None))
 pid2find = "10.18710/TLA01U"
@@ -22,4 +40,4 @@ ret = '<a href="https://doi.org/' + \
 
 print("generated html line: ", ret)
 
-imgkit.from_string(ret, "test.png")
+imgkit.from_string(ret, "test.png", options=options)
